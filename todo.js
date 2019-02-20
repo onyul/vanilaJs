f99b31fd849494c1ecd613bae7d95827ce439cec
onyul <heogeuns@gmail.com>
2019-02-20 14:23:20 +0900

    [2019.02.20]todolist 삭제버튼 기능 추가 작업 Please enter the commit message for your changes. Lines starting

--- todo.js
+++ todo.js
@@ -3,7 +3,19 @@ const toDoForm = document.querySelector('.js-toDoForm'),
 	toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event){
+	const btn = event.target;
+	const li = btn.parentNode;
+	toDoList.removeChild(li);
+	const cleanToDos = toDos.filter(function(toDo){		
+		return  toDo.id !== parseInt(li.id);
+	});
+	// console.log(cleanToDos);
+	toDos = cleanToDos;
+	saveToDos();
+}
 
 function saveToDos(){
 	localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
@@ -16,6 +28,7 @@ function paintToDo(text){
 	const newId = toDos.length+1;
 
 	delBtn.innerText="X";
+	delBtn.addEventListener('click',deleteToDo)
 	span.innerText = text;
 	li.appendChild(delBtn);
 	li.appendChild(span);
@@ -44,7 +57,6 @@ function loadToDos(){
 			paintToDo(toDo.text);
 		});
 	}
-	
 }
 
 function init(){
